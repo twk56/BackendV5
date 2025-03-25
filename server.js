29ca83d03450ba0e60448331f94bf90e8774b6ba
twk56 <thanawatklomkliew@gmail.com>
2025-03-25 22:21:07 +0700

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(limiter);
 connectDB();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: '*',
   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
